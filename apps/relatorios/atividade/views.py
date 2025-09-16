15076a2b82b3505b73025c6ebfef3aeb81c0c839
MateusMSoares <mateusmsoares@protonmai.com>
2025-09-16 20:43:34 -0300

    ATHOS-13 fix(Service): Adicionado anotacao para seguranca do metodos de GET criados para o service

--- apps/relatorios/atividade/views.py
+++ apps/relatorios/atividade/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from apps.relatorios.atividade.services import AtividadeService
+from django.views.decorators.http import require_GET
 
 def index(request):
     return render(request, 'atividade/index.html')
-
+@require_GET
 def relatorio_horas_por_dev(request):
     mes = request.GET.get('mes')
 
@@ -15,6 +16,7 @@ def relatorio_horas_por_dev(request):
     dados = service.soma_horas_por_dev_por_mes(mes)
     return JsonResponse(dados, safe=False)
 
+@require_GET
 def relatorio_horas_por_projeto(request):
     mes = request.GET.get('mes')
 
