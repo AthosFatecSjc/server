48a4d6ebf9a2d0677074735d62d22b2d9d62193d
Renato Mendes <111528529+RenatoCMMendes@users.noreply.github.com>
2025-09-18 20:38:49 -0300

    Update urls.py

--- apps/relatorios/comparacao/urls.py
+++ apps/relatorios/comparacao/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('relatorio-anual/json/', views.relatorio_anual_comparacao_json, name='relatorio_anual_comparacao_json'),
+    path('', views.index, name='comparacao_index'),
 ]
