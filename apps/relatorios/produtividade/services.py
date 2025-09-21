a88f18bda5896ae2fc0ef8227c89253c04903dd6
JuanCursino <cursinojones@gmail.com>
2025-09-20 22:46:06 -0300

    ATHOS-10 feat(integração)Criar a tabela calendário no front (front + integracao)

--- apps/relatorios/produtividade/services.py
+++ apps/relatorios/produtividade/services.py
@@ -7,6 +7,7 @@ def calcular_spends_por_dev(mes, ano):
 
     total_real = 0.0
     total_meta = 0.0
+    soma_diaria_total = {d: 0.0 for d in range(1, 32)}
 
     for func in funcionarios:
         registros = TempoGastoEquipe.objects.filter(
@@ -18,21 +19,22 @@ def calcular_spends_por_dev(mes, ano):
         dias_por_func = {}
         for r in registros:
             dia = r['dia_mes']
-            dias_por_func[dia] = float(r['total_horas'])
+            horas_dia = float(r['total_horas'])
+            dias_por_func[dia] = round(horas_dia, 1)
+            soma_diaria_total[dia] += horas_dia
 
         for d in range(1, 32):
             if d not in dias_por_func:
                 dias_por_func[d] = 0.0
 
         horas_real = sum(dias_por_func.values())
-
         meta = 154.0
         percentual = (horas_real / meta * 100) if meta > 0 else 0
 
         resultados.append({
             "funcionario": func.nome,
             "dias": dias_por_func,
-            "real": horas_real,
+            "real": round(horas_real, 1),
             "meta": meta,
             "percentual": round(percentual, 1)
         })
@@ -41,10 +43,17 @@ def calcular_spends_por_dev(mes, ano):
         total_meta += meta
 
     percentual_total = (total_real / total_meta * 100) if total_meta > 0 else 0
+    
+
+    soma_diaria_total_arredondada = {}
+    for dia, valor in soma_diaria_total.items():
+        soma_diaria_total_arredondada[dia] = round(valor, 1)
+    
+
     resultados.append({
-        "funcionario": "TOTAL",
-        "dias": {d:0.0 for d in range(1, 32)},
-        "real": total_real,
+        "funcionario": "REALIZADO",
+        "dias": soma_diaria_total_arredondada, 
+        "real": round(total_real, 1),
         "meta": total_meta,
         "percentual": round(percentual_total, 1)
     })
