526e59cd7c2e9fadd1176626bc57e37797c6524d
Trevizoli <rtrevizoli@yahoo.com.br>
2025-10-13 23:39:59 -0300

    ATHOS-159 chore(env): Adiciona allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -29,8 +29,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost'])
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
 
 # Application definition
 
