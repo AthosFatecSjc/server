8678194ba2719ab0f0c0aa5b4d5c76cf7f504e22
Ruth Mira <ruth.mira@fatec.sp.gov.br>
2025-10-23 15:28:48 -0300

    ATHOS-184 style(produtividade_projeto): Adiciona bloco de Dashboard de produtividade e retira exportar no _header

--- apps/dashboards/desenvolvedores/views.py
+++ apps/dashboards/desenvolvedores/views.py
@@ -4,4 +4,11 @@ from django.views.decorators.http import require_http_methods
 
 @require_http_methods(["GET"])
 def index(request):
-    return render(request, "desenvolvedores/index.html")
+    header_context = {
+        "title": "Gerenciamento de Desenvolvedores",
+        "subtitle": "Configure valores por hora e visualize estatísticas",
+        "breadcrumb": "Gerenciamento",
+    }
+    return render(
+        request, "desenvolvedores/index.html", {"header_context": header_context}
+    )
