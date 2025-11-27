d7d392d1bae0711f413f4fe83eec5b1ac1447664
Ruth Mira <ruth.mira@fatec.sp.gov.br>
2025-11-27 00:04:58 -0300

    ATHOS-227 fix(usuario): ajuste no permissionamento de exclusao e desativacao

---
+++ apps/usuarios/migrations/0005_remove_seed_users_again.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+
+def remove_seed_users(apps, schema_editor):
+    usuario_model = apps.get_model("usuarios", "Usuario")
+    usernames = ["gerente", "lider", "membro"]
+    usuario_model.objects.filter(username__in=usernames).delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("usuarios", "0004_remove_seed_users"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            remove_seed_users,
+            reverse_code=migrations.RunPython.noop,
+        ),
+    ]
